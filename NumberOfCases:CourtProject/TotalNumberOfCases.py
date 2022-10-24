dcaa47f67b79f3a5fab4f52493b63712cc90ca59
lojnys <lojnys@students.cs.ubc.ca>
2022-10-23 20:16:38 -0700

    edited model and added visualization to the data

--- NumberOfCases:CourtProject/TotalNumberOfCases.py
+++ NumberOfCases:CourtProject/TotalNumberOfCases.py
@@ -4,21 +4,22 @@ import pandas as pd
 """This project is for displaying a graph of adult criminal courts, number of cases
 and cahrges by type of decision. The data is extracted from Statistics Canada."""
 
-class Data(): 
+class Data: 
 
-    filename = '/Users/yushinnam/Desktop/python3/DataVisualizationPersonalProject/NumberOfCases:CourtProject/data_total.csv'
-    df = pd.read_csv(filename)
+    def __init__(self, filename: str): 
+        self.filename = filename
+        self.df = pd.read_csv(filename)
 
 
     # EFFECTS: Extracting years from the data as a list (to iterate)
-    def getYears() -> list:
+    def getYears(self) -> list:
         """a method to extract a list of years from the data"""
         """this method can be used for x values of the graph"""
 
         year_list = []
-        for i in range(0, len(df.index)):
-            if not(df['REF_DATE'][i] in year_list): 
-                year_list.append(df['REF_DATE'][i])
+        for i in range(0, len(self.df.index)):
+            if not(self.df['REF_DATE'][i] in year_list): 
+                year_list.append(self.df['REF_DATE'][i])
         
         return year_list
 
@@ -26,7 +27,7 @@ class Data():
     # EFFETS: Classifying data by year
     # REQUIRES: year > 0 and 
     #           the number of elements in typeOfDecisionList and classified_list must be the same
-    def classifyByYear(year: int) -> list:
+    def classifyByYear(self, year: int) -> list:
         year1 = str(year)
         year2 = str(year+1)
         classified_list = []
@@ -34,9 +35,9 @@ class Data():
         typeOfDecisionList = ["Total decisions", "Guilty", "Percentage guilty", "Acquitted", "Stayed or withdrawn", "Other decisions"]
         final_list = [f'{year1}/{year2}']
 
-        for i in range(0, len(df.index)):
-            if df['REF_DATE'][i] == f'{year1}/{year2}':
-                classified_list.append(df['VALUE'][i])
+        for i in range(0, len(self.df.index)):
+            if self.df['REF_DATE'][i] == f'{year1}/{year2}':
+                classified_list.append(self.df['VALUE'][i])
 
 
         for j in range(0, len(typeOfDecisionList)): 
@@ -48,14 +49,14 @@ class Data():
 
 
     # EFFECTS: gets the values of the given type of decision
-    def getValues(type: str) -> list:
+    def getValues(self, type: str) -> list:
         """This method can be used for the y values of the graph"""
 
         classfied_values = []
 
-        for i in range(0, len(df.index)): 
-            if (df["Type of decision"][i] == type):
-                classfied_values.append(df['VALUE'][i])
+        for i in range(0, len(self.df.index)): 
+            if (self.df["Type of decision"][i] == type):
+                classfied_values.append(self.df['VALUE'][i])
 
         return classfied_values
 
---
+++ NumberOfCases:CourtProject/TotalNumberOfCasesVisual.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+from TotalNumberOfCases import Data
+
+if __name__ == "__main__":
+    filename = '/Users/yushinnam/Desktop/python3/DataVisualizationPersonalProject/NumberOfCases:CourtProject/data_total.csv'
+    data = Data(filename)
+
+    type = input("Which type of decisions would you like to see? ")
+
+    x_values = data.getYears()
+    y_values = data.getValues(type)
+
+    plt.style.use('seaborn')
+    fig, ax = plt.subplots()
+    plt.plot(x_values, y_values, linewidth=3)
+    
+    plt.show()
